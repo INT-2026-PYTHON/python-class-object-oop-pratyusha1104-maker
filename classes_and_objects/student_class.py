54d38e496c96372d0abe03ebc90dcb872af3ef18
pratyusha1104 <pratyusha1104@gmail.com>
2026-06-11 15:47:06 +0530

    Assignment Completed!

--- classes_and_objects/student_class.py
+++ classes_and_objects/student_class.py
@@ -79,3 +79,64 @@ Explanation:
 =================================================
 
 """
+class Student:
+    def __init__(self, name, roll, marks=None):
+        self.name = name
+        self.roll = roll
+
+        if marks is None:
+            self.marks = []
+        else:
+            self.marks = marks
+
+    def add_mark(self, mark):
+        if mark < 0 or mark > 100:
+            print(f"Invalid mark: {mark}")
+        else:
+            self.marks.append(mark)
+
+    def total(self):
+        return sum(self.marks)
+
+    def average(self):
+        if len(self.marks) == 0:
+            return 0
+        return self.total() / len(self.marks)
+
+    def grade(self):
+        avg = self.average()
+
+        if avg >= 90:
+            return "A"
+        elif avg >= 75:
+            return "B"
+        elif avg >= 50:
+            return "C"
+        else:
+            return "F"
+
+    def report(self):
+        return (
+            self.name,
+            self.roll,
+            self.total(),
+            self.average(),
+            self.grade()
+        )
+    
+students = []
+n = int(input("Enter number of students: "))
+for i in range(n):
+    print("\nStudent", i + 1)
+    name = input("Enter name: ")
+    roll = int(input("Enter roll number: "))
+    s = Student(name, roll)
+    marks=list(map(int,input("Enter marks sepatred by comma: ").split(",")))
+    for mark in marks:
+        s.add_mark(mark)
+
+    students.append(s)
+
+print("\n--- Student Reports ---")
+for student in students:
+    print(student.report())
